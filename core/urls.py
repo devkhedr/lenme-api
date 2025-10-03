02a4866f24283a0f0a1a37b8a122734d09b4691c
devkhedr <mohamed.khedr.dev@gmail.com>
2025-10-03 06:04:31 +0300

    fix: adjust swagger path

--- core/urls.py
+++ core/urls.py
@@ -8,7 +8,7 @@ schema_view = get_schema_view(
     openapi.Info(
         title="Lenme API Documentation",
         default_version="v1",
-        description="Welcome to the Lenme API Documentation! We are thrilled to have you here and are eager to help you get started with integrating our platform into your projects.",
+        description="Welcome to the Lenme API Documentation!",
     ),
     public=True,
     permission_classes=[permissions.AllowAny],
@@ -18,5 +18,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/lending/", include("lending.urls")),
     path("api/payment/", include("payment.urls")),
-    path("docs/", schema_view.with_ui("swagger"), name="swagger-docs"),
+    path("", schema_view.with_ui("swagger"), name="swagger-docs"),
 ]
